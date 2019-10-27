f794b2f61c99d7d8d7b963be6bbae62108aea6f6
Pham Van Lam <phamvanlam1991@gmail.com>
2019-10-27 21:35:19 +0700

    add Class NewStudentForm

---
+++ src/main/java/com/Bean/NewStudentForm.java
@@ -0,0 +1,31 @@
+package com.Bean;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import javax.faces.bean.SessionScoped;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SessionScoped
+public class NewStudentForm implements Serializable {
+
+	private static final long serialVersionUID = -2615270572543229725L;
+
+	private String code;
+	private String firstName;
+	private String lastName;
+	private Date dob;
+	private String gender;
+	private String field;
+	private String address;
+	private String phone;
+	private String email;
+
+}
