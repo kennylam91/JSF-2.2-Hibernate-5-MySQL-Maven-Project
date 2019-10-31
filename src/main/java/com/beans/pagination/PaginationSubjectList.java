120b331f13107e9ccd11f16f5cb396580cacba80
Pham Van Lam <phamvanlam1991@gmail.com>
2019-10-31 15:11:14 +0700

    refactor subject list

---
+++ src/main/java/com/beans/pagination/PaginationSubjectList.java
@@ -0,0 +1,12 @@
+package com.beans.pagination;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "paginationSubjectList")
+@SessionScoped
+public class PaginationSubjectList extends Pagination {
+
+	private static final long serialVersionUID = 2256444442440214309L;
+
+}
