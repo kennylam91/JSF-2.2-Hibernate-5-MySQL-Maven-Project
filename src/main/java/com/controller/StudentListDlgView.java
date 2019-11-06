0c7783bfde3f2b90e6e5b5a04f6e61959b0f0760
Pham Van Lam <phamvanlam1991@gmail.com>
2019-11-06 14:11:03 +0700

    pagination ok

--- src/main/java/com/controller/StudentListDlgView.java
+++ src/main/java/com/controller/StudentListDlgView.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -9,6 +10,7 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 import org.apache.log4j.Logger;
+import org.primefaces.event.data.SortEvent;
 
 import com.beans.StudentDto;
 import com.beans.pagination.Pagination;
@@ -27,12 +29,10 @@ import lombok.Setter;
 public class StudentListDlgView implements Serializable {
 
 	private static final long serialVersionUID = 6318963726091789725L;
-
 	private static final Logger logger = Logger.getLogger(StudentController.class);
-
 	private List<StudentDto> studentDtos;
-
 	private Pagination pagination;
+	private List<StudentDto> selectedStudentDtos;
 
 	@ManagedProperty(value = "#{studentService}")
 	private StudentService studentService;
@@ -41,6 +41,30 @@ public class StudentListDlgView implements Serializable {
 	public void init() {
 		pagination = new PaginationStudentList();
 		studentDtos = studentService.findStudentsByPagination(pagination);
+	}
+	
+	public void onPaginationChange() {
+		studentDtos = studentService.findStudentsByPagination(pagination);
+	}
+	
+	public void getPreviousPage() {
+		pagination.setPage(pagination.getPage()-1);
+		onPaginationChange();
+	}
+	
+	public void getNextPage() {
+		pagination.setPage(pagination.getPage()+1);
+		onPaginationChange();
+	}
+	
+	public void onSort(SortEvent event) {
+		if (event.isAscending()) {
+			pagination.setAscOrDesc("asc");
+		} else {
+			pagination.setAscOrDesc("desc");
+		}
+		pagination.setOrderBy(event.getSortColumn().getField());
+		onPaginationChange();
 	}
 
 }
